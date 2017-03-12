a61dda95fb392c72621af0877b61f65d13e32cea
Gabriela Surita <gabsurita@gmail.com>
2017-03-12 14:07:46 -0300

    sketch funcionalties

---
+++ kinto_migrations/__init__.py
@@ -0,0 +1,49 @@
+def migrate_model(client, model):
+    """Create or update Kinto resources specified on a model object.
+
+    :param client: Kinto http client.
+    :param model: Mapping representing a kinto model.
+    """
+
+    buckets = model.get('$buckets', {})
+
+    with client.batch() as batch:
+        for bucket_id, bucket in buckets.items():
+
+            groups = bucket.pop('$groups', {})
+            collections = bucket.pop('$collections', {})
+            permissions = bucket.pop('$permissions', {})
+
+            batch.update_bucket(bucket_id,
+                                data=bucket,
+                                permissions=permissions)
+
+            for group_id, group in groups.items():
+
+                permissions = group.pop('$permissions', {})
+                group.setdefault('members', [])
+
+                batch.update_group(group_id,
+                                   bucket=bucket_id,
+                                   data=group,
+                                   permissions=permissions)
+
+            for collection_id, collection in collections.items():
+
+                records = collection.pop('$collections', {})
+                permissions = collection.pop('$permissions', {})
+
+                batch.update_collection(collection=collection_id,
+                                        bucket=bucket_id,
+                                        data=collection,
+                                        permissions=permissions)
+
+                for record_id, record in records.items():
+
+                    permissions = record.pop('$permissions', {})
+
+                    batch.update_record(record_id,
+                                        bucket=bucket_id,
+                                        collection=collection_id,
+                                        data=group,
+                                        permissions=permissions)
---
+++ kinto_migrations/__main__.py
@@ -0,0 +1,31 @@
+import argparse
+import logging
+
+import yaml
+from kinto_http import cli_utils
+
+from . import migrate_model
+
+# Setup parser
+parser = argparse.ArgumentParser(
+    description='Migrate Kinto databases from specification files.'
+)
+
+parser.add_argument('-f', '--file',
+                    help='File describing Kinto resources.')
+
+cli_utils.add_parser_options(parser)
+args = parser.parse_args()
+
+# Setup logging
+logger = logging.getLogger(__name__)
+cli_utils.setup_logger(logger, args)
+
+# Create client
+client = cli_utils.create_client_from_args(args)
+
+# Load models file
+models = yaml.load(open(args.file or 'models.yml'))
+
+# Run migration
+migrate_model(models)
