4cd0510ef23b823bffa40a19d82f7e785170c3d6
Gabriela Surita <gabsurita@gmail.com>
2017-03-12 16:23:05 -0300

    add setup file

---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import codecs
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    readme = f.read()
+
+with codecs.open(os.path.join(here, 'CHANGELOG.rst'), encoding='utf-8') as f:
+    history = f.read()
+
+requirements = [
+    'kinto_http',
+    'pyaml',
+]
+
+test_requirements = [
+    'mock',
+    'unittest2',
+]
+
+setup(
+    name='kinto-migrations',
+    version='0.1.0',
+    description="Create Kinto ",
+    long_description=readme + '\n\n' + history,
+    author="Gabriela Suria",
+    author_email='gabsurita@gmail.com',
+    url='https://github.com/gabisurita/kinto-migrations',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    license="Apache License (2.0)",
+    zip_safe=False,
+    keywords='kinto',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
